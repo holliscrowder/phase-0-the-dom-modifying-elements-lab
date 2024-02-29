65f24937d1d1d59bc9ed0adbce2ceb45d6fa0ce3
Hollis Crowder <hollis.crowder@gmail.com>
2024-02-28 18:33:41 -0800

    complete DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// remove main //
+const main = document.getElementById("main");
+main.remove();
+
+// create new header variable 'h1' //
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Hollis is the champion";
+document.head.append(newHeader);
